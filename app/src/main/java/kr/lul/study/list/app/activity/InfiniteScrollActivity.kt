d8b12a5ecf2bf270b44c96fc5f09bf3f9c89029b
Just Burrow <just.burrow@lul.kr>
2022-07-31 20:25:09 +0900

    ✨ 목록 마지막까지 스크롤 한 것을 인식.

--- app/src/main/java/kr/lul/study/list/app/activity/InfiniteScrollActivity.kt
+++ app/src/main/java/kr/lul/study/list/app/activity/InfiniteScrollActivity.kt
@@ -9,10 +9,12 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -49,17 +51,54 @@ fun InfiniteScrollActivityLayout(
     initContents: List<Data> = listOf(),
     viewModel: InfiniteScrollViewModel = viewModel()
 ) {
+    val listState = rememberLazyListState()
     LazyColumn(
         modifier = Modifier
             .fillMaxWidth()
             .fillMaxHeight(),
-        contentPadding = PaddingValues(3.dp)
+        contentPadding = PaddingValues(3.dp),
+        state = listState
     ) {
         val items = initContents.ifEmpty { viewModel.load() }
         Log.v(TAG, "#layout : items=$items")
         items(items) {
             DataCard(it)
         }
+    }
+
+    listState.OnBottomReached {
+        Log.v(TAG, "#onBottomReached")
+        viewModel.load()
+    }
+}
+
+/**
+ * - 다음 데이터를 읽어야 하는지 판단.
+ * - 다음 데이터 로딩.
+ */
+@Composable
+fun LazyListState.OnBottomReached(
+    loadMore: () -> Unit
+) {
+    /**
+     * 다음 데이터를 로딩해야 하는지 판단.
+     */
+    val shouldLoadMore = remember {
+        derivedStateOf {
+            val lastVisible = layoutInfo.visibleItemsInfo.lastOrNull()
+                ?: return@derivedStateOf true
+            lastVisible.index == layoutInfo.totalItemsCount - 1
+        }
+    }
+
+    // 실행시 `shouldLoadMore`를 모니터링.
+    LaunchedEffect(shouldLoadMore) {
+        snapshotFlow { shouldLoadMore.value }
+            .collect {
+                if (it) { // 다음 로딩 상태가 `true`가 되면
+                    loadMore() // 새 데이터 로딩.
+                }
+            }
     }
 }
 
